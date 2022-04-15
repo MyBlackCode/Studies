230e7880683e294bbab9ed484416a0a82e03b962
MyBlackCode <paula.mlynarczyk@op.pl>
2022-04-15 17:24:22 +0200

    Get the contents of the text file into a string variable. Define the type of the variable before downloading the file contents.

---
+++ OperatingSystems/PowerShell2/Exercise2.ps1
@@ -0,0 +1,9 @@
+<# Exercise2
+PL
+Pobierz zawartość pliku tekstowego do zmiennej typu string. Zdefiniuj typ zmiennej
+przed pobraniem zawartości pliku.
+ENG
+Get the contents of the text file into a string variable. Define the type of the variable
+before downloading the file contents.#>
+
+$variable = Get-Content -Path C:\Users\User\MyRepositoriesGH\OperatingSystems\Powershell2\Miesiące.txt
