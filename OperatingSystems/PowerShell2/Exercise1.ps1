98ec9d0afedaa5dcacdbba92e5c00e90fb802db7
MyBlackCode <paula.mlynarczyk@op.pl>
2022-04-15 20:34:21 +0200

    Create a text file containing the names of the next months of the year, each month in a new line of the document. Use streams for this purpose.

---
+++ OperatingSystems/PowerShell2/Exercise1.ps1
@@ -0,0 +1,9 @@
+<# Exercise1 
+PL
+Utwórz plik tekstowy, w którym znajdować się będą nazwy kolejnych miesięcy w roku,
+każdy miesiąc w nowej linii dokumentu. Skorzystaj w tym celu ze strumieni.
+ENG
+Create a text file containing the names of the next months of the year,
+each month in a new line of the document. Use streams for this purpose.#>
+
+"Styczeń`nLuty`nMarzec`nKwiecień`nMaj`nCzerwiec`nLipiec`nSierpień`nWrzesień`nPaździernik`nListopad`nGrudzień" > Marzec.txt
