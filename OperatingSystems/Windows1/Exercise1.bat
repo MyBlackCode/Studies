e9775e094cecdbe67429c9a126a0060b023706bf
MyBlackCode <101878681+MyBlackCode@users.noreply.github.com>
2022-04-16 23:22:13 +0200

    Operating Systems Windows1 Exercise1

--- OperatingSystems/Windows1/Exercise1.bat
+++ OperatingSystems/Windows1/Exercise1.bat
@@ -1,7 +1,7 @@
 :: Exercise1
 :: PL
-:: Zmień tytuł wiersza poleceń na tekst,, Proszę czekać trwa formatowanie dysku C: ’’
+:: Zmień tytuł wiersza poleceń na tekst "Proszę czekać trwa formatowanie dysku C:"
 :: ENG
-:: Zmień tytuł wiersza poleceń na tekst,, Proszę czekać trwa formatowanie dysku C: ’’
+:: Change the command line title to the text "Please wait, the C: drive is being formatted"
 
 title "Proszę czekać trwa formatowanie dysku C:"
