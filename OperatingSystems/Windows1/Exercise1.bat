e5ff3d418df4a2a74d753379582bb8ea6d31bb17
MyBlackCode <paula.mlynarczyk@op.pl>
2022-04-16 23:18:31 +0200

    Operating Systems Windows1 Exercise1

---
+++ OperatingSystems/Windows1/Exercise1.bat
@@ -0,0 +1,7 @@
+:: Exercise1
+:: PL
+:: Zmień tytuł wiersza poleceń na tekst,, Proszę czekać trwa formatowanie dysku C: ’’
+:: ENG
+:: Zmień tytuł wiersza poleceń na tekst,, Proszę czekać trwa formatowanie dysku C: ’’
+
+title "Proszę czekać trwa formatowanie dysku C:"
