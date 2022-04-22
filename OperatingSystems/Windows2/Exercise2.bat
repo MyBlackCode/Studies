df2e9c9e4249c8c4563aeed3cce0cda08ecbc884
MyBlackCode <paula.mlynarczyk@op.pl>
2022-04-22 15:29:17 +0200

    Windows2 Exercises

---
+++ OperatingSystems/Windows2/Exercise2.bat
@@ -0,0 +1,8 @@
+::Exercise2
+::PL
+::Uruchom konsolę tak,aby były wyświetlane czerwone napisy na białym tle
+::ENG
+::Start the console so that the red text on a white background is displayed
+
+@echo off
+color 74
