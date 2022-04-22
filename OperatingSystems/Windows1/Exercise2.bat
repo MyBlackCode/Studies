bd2d0474fa95e87a90732af767efc625e2b6d067
MyBlackCode <101878681+MyBlackCode@users.noreply.github.com>
2022-04-22 15:24:35 +0200

    Delete OperatingSystems/Windows1 directory

--- OperatingSystems/Windows1/Exercise2.bat
+++
@@ -1,8 +0,0 @@
-::Exercise2
-::PL
-::Uruchom konsolę tak,aby były wyświetlane czerwone napisy na białym tle
-::ENG
-::Start the console so that the red text on a white background is displayed
-
-@echo off
-color 74
