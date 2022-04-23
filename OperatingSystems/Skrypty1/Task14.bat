916a76f332377aec4bb6eb77131bea7e3fac777e
MyBlackCode <paula.mlynarczyk@op.pl>
2022-04-23 17:07:03 +0200

    Skrypt1 Tasks

--- OperatingSystems/Skrypty1/Task14.bat
+++ OperatingSystems/Skrypty1/Task14.bat
@@ -6,22 +6,20 @@
 ::Skrypt może zostać oparty zarówno na pętli for jak i instrukcji skoku goto.
 @echo off
 echo Task14
-set /P n=Podaj liczbe liczb do wygenerowania od 1 do 10: 
+set /P n=Podaj liczbe liczb do wygenerowania od 1 do 10:
+SETLOCAL EnableDelayedExpansion
 if %n% LEQ 10 (
     if %n% GEQ 1 (
 	set /P s=Podaj maksymalna wartosc jaka ma zostac wygenerowana:
-	SETLOCAL EnableDelayedExpansion
 	for /L %%i in (%n%,-1,1) do (
-	    set /A liczba=%random%%%%s%+1
+	    set /A liczba=%random%%%!s!+1
 	    echo !liczba!
 	)
-	SETLOCAL DisableDelayedExpansion	
     ) else (
 	echo Podano za maly parametr
     )
 ) else (
     echo Podano za duzy parametr
 )
-::echo.
-set /A liczba=%random%%%10+1
-echo %liczba%
+SETLOCAL DisableDelayedExpansion
+echo.
