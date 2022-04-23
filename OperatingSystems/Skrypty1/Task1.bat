c76aa08572b5e537c4ff3a2f0cc8ed7aaee359bd
MyBlackCode <paula.mlynarczyk@op.pl>
2022-04-23 11:39:55 +0200

    Skrypty1 Exercises

---
+++ OperatingSystems/Skrypty1/Task1.bat
@@ -0,0 +1,7 @@
+::Zadanie1.
+::Utworzyć skrypt o treści z powyższego zrzutu ekranu i uruchomić (klikając na ikonę skrypt.bat).
+::Usunąć pierwszą linijkę skryptu i spróbować uruchomić ponownie. Następnie usunąć ostatnią 
+::linijkę i uruchomić. 
+@ echo off
+echo Hello World 
+pause
