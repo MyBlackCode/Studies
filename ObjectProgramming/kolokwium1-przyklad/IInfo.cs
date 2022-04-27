513af3ac616b5b7e684d5a1438db813d9868958a
MyBlackCode <paula.mlynarczyk@op.pl>
2022-04-27 20:09:50 +0200

    interface

---
+++ ObjectProgramming/kolokwium1-przyklad/IInfo.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace kolokwium1_przyklad
+{
+    interface IInfo
+    {
+        public void WypiszInfo();
+    }
+}
