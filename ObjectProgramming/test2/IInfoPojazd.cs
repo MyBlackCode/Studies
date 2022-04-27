ab768667f32024bb9024fcdd58f6d7ef82a97408
MyBlackCode <paula.mlynarczyk@op.pl>
2022-04-27 23:38:24 +0200

    Interface

---
+++ ObjectProgramming/test2/IInfoPojazd.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace test2
+{
+    public interface IInfoPojazd : IInfo
+    {
+        public float MasaCalkowita();
+        public int Pasazerow();
+    }
+}
