5e8be098b9e2124708cd37eea4b466fd0a2665e7
MyBlackCode <paula.mlynarczyk@op.pl>
2022-08-16 20:59:58 +0200

    update

--- ProgrammingParadigms/FS_3/FS_3/Program.fs
+++ ProgrammingParadigms/FS_3/FS_3/Program.fs
@@ -135,3 +135,81 @@ let NajdluzszyNajkrotszyWyraz (lista:Lista<string>)=
     (max,min)
 
 Console.WriteLine($"Lista [{l}] -> najdłuższe słowo ma: {fst (NajdluzszyNajkrotszyWyraz (l))} liter, a najkrótsze: {snd (NajdluzszyNajkrotszyWyraz (l))}")
+
+//3.11 Napisz fukcję, która będzie przyjmowała listę stringów reprezentujących polskie imiona
+//i zwróci tylko listę imion źeńskich
+Console.WriteLine("\n3.11")
+let rec FiltrujImionaZenskie (lista:Lista<string>)=
+    match lista with
+        | Pusta -> Pusta
+        | Wezel (glowa, ogon) ->
+            if glowa[glowa.Length-1]='a' then 
+                Wezel(glowa, FiltrujImionaZenskie ogon)
+            else 
+                FiltrujImionaZenskie ogon
+
+
+let imiona =Wezel("Julia",Wezel("Paweł",Wezel("Alicja",Wezel("Krystian", Wezel("Bartek", Wezel("Magda", Pusta))))))
+Console.WriteLine($"Lista imon [{imiona}] -> Lista imion zenskich: [{FiltrujImionaZenskie imiona}]")
+
+//3.12 Napisz funkcję, która będzie odracała kolejność elementów na liście.
+Console.WriteLine("\n3.12")
+let rec OdwrocListe (lista : Lista<'a>, odwroconaLista : Lista<'a>) : (Lista<'a>) = 
+        match lista with
+        | Wezel(glowa, ogon) ->  OdwrocListe(ogon, Wezel(glowa, odwroconaLista))
+        | Pusta ->  odwroconaLista
+        
+Console.WriteLine($"{(lista)} \t [3] -> {(OdwrocListe (lista, Pusta))}")
+
+//3.13 Napisz funkcję, która będzie przyjmowała listę stringów reprezentujących polskie
+//imiona i zwróci dwie listy: osobno listę imion żeńskich oraz listę imion męskich.
+Console.WriteLine("\n3.13")
+let rec FiltrujImiona (lista : Lista<string>, listaZenska: Lista<string>, listaMeska: Lista<string> )=
+    match lista with
+        | Pusta -> (OdwrocListe (listaZenska, Pusta), OdwrocListe (listaMeska, Pusta))
+        | Wezel (glowa, ogon) ->
+            if glowa[glowa.Length-1]='a' then 
+                FiltrujImiona(ogon, Wezel(glowa, listaZenska), listaMeska)
+            else 
+                 FiltrujImiona(ogon, listaZenska, Wezel(glowa,listaMeska))
+
+Console.WriteLine($"Lista imon [{imiona}] \nLista imion żeńskich: [{fst (FiltrujImiona (imiona, Pusta, Pusta))}]")
+Console.WriteLine($"Lista imion męskich: [{snd (FiltrujImiona (imiona, Pusta, Pusta))}]")
+
+//3.14 Napisz funkcję, która będzie przyjmowała dwie listy liczb całkowitych i zwracała listę
+//wartości logicznych, gdzie true określa, że liczba na pierwszej liście była większa, a false,
+//że wartość na drugiej liście była większa. Jeżeli jedna lista jest dłuższa od drugiej zwróć
+//wyjątek informujący o tym fakcie.
+Console.WriteLine("\n3.14")
+let rec CzyPierwszaListaWieksza (liata1 : Lista<int>, lista2 : Lista<int>) : (Lista<Boolean>) = 
+        match liata1, lista2 with
+        | Wezel(glowa1, ogon1), Wezel(glowa2, ogon2) ->  Wezel(glowa1 > glowa2, CzyPierwszaListaWieksza(ogon1, ogon2))
+        | Pusta, Pusta  ->  Pusta
+        | _  ->  failwith "nierówne listy"
+        
+
+let lista2 = OdwrocListe(lista, Pusta)
+Console.WriteLine($"[{lista}] > [{lista2}] = [{(CzyPierwszaListaWieksza (lista, lista2))}]")
+
+//3.15 Napisz funkcję, która będzie przyjmowała dwie listy liczb całkowitych i zwracała listę
+//wartości zdefiniowanego przez ciebie typu wyliczeniowego, gdzie Pierwsza określa, że liczba
+//na pierwszej liści była większa, a Druga, że wartość na drugiej liście była większa. Jeżeli 
+//jedna lista się skończy, to generowanie listy  wejściowej ma trwać dalej dopóki są elementy
+//na drugiej liście. W tym przypadku przyjmujemy, że istniejący element  jest większy  niż 
+//nieistniejący.
+Console.WriteLine("\n3.15")
+type Enum =
+    | Pierwszy = 'P'
+    | Drugi = 'D'
+
+let rec CzyPierwszaListaWieksza2 (liata1 : Lista<int>, lista2 : Lista<int>) = 
+        match liata1, lista2 with
+        | Wezel(glowa1, ogon1), Wezel(glowa2, ogon2) when glowa1 > glowa2 ->  Wezel(Enum.Pierwszy, CzyPierwszaListaWieksza2(ogon1, ogon2))
+        | Wezel(glowa1, ogon1), Wezel(glowa2, ogon2) ->  Wezel(Enum.Drugi, CzyPierwszaListaWieksza2(ogon1, ogon2))
+        | Wezel(glowa1, ogon1), Pusta -> Wezel(Enum.Pierwszy, CzyPierwszaListaWieksza2(ogon1, Pusta))
+        | Pusta, Wezel(glowa2, ogon2) -> Wezel(Enum.Drugi, CzyPierwszaListaWieksza2(Pusta, ogon2))
+        | Pusta, Pusta  ->  Pusta
+        | _  ->  failwith "nierówne listy"
+
+let lista3 = ListaLiczbNaturalnych 7
+Console.WriteLine($"[{lista3}] > [{lista2}] = [{(CzyPierwszaListaWieksza2 (lista3, lista2))}]")
