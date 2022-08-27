e4594994f143ec3c221261668c2d618b2ae29251
MyBlackCode <paula.mlynarczyk@op.pl>
2022-08-28 00:27:11 +0200

    update 00:27 28:08.2022

---
+++ ProgrammingParadigms/CS_3_Example2/CS_3_Example2/OperacjeNaLiscie.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CS_3_Example2
+{
+    internal class OperacjeNaLiscie
+    {
+        public static List<T> Filtr<T> (List<T> lista, Func<T,bool> warunek)
+        {
+            List<T> result = new List<T>();
+            for(int i = 0; i < lista.Count; i++)
+                if(warunek(lista[i]))
+                    result.Add(lista[i]);   
+            return result;
+        }
+    }
+}
---
+++ ProgrammingParadigms/CS_3_Example2/CS_3_Example2/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace CS_3_Example2
+{
+    internal class Program
+    {
+        static bool TylkoDlugie(string wartosc) => wartosc.Length > 18;
+
+        static Func<string, bool> TylkNaLitere(char litera) => (string wartosc) => wartosc[0] == litera;
+        static void Main(string[] args)
+        {
+            List<string> przedmioty = new List<string>
+            {
+                "Paradygmaty programowania",
+                "Programowanie Stron internetowych",
+                "Algebra",
+                "Logika dla informatyków",
+                "Sieci komputerowe"
+            };
+
+            List<string> dlugieNazwy = OperacjeNaLiscie.Filtr(przedmioty, TylkoDlugie);
+            List<string> przedmiotyNaLitereP = OperacjeNaLiscie.Filtr(przedmioty, TylkNaLitere('P'));
+            List<string> przedmiotyNaLitereA = OperacjeNaLiscie.Filtr(przedmioty, TylkNaLitere('A'));
+
+        }
+    }
+}
