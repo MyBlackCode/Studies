2ea185d3725b312d48379dfe3d059a60216ec1c7
MyBlackCode <paula.mlynarczyk@op.pl>
2022-10-18 20:16:08 +0200

    created 20:16 18.10.2022

---
+++ ParallelSystemsAndAlgorithms/PSAA_L2_T1.cpp
@@ -0,0 +1,33 @@
+//Zad2
+//Napisac program, w którym proces 0 pobiera ze standardowego wejscia
+//liczbe typu double(std::cin » wrt;).Nastepnie proces ten wysy³a wczytana
+//liczbe do procesu 1. Proces 1 ma za zadanie wyswietlic ta liczbe na
+//standardowym wyjsciu.
+
+#include <iostream>
+#include <mpi.h>
+
+int main(int argc, char* argv[])
+{
+
+    MPI_Init(&argc, &argv);
+    int rank, size;
+    MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+    MPI_Comm_size(MPI_COMM_WORLD, &size);
+
+    if (rank == 0)
+    {
+        double wrt;
+        std::cin >> wrt;
+        MPI_Send(&wrt, 1, MPI_DOUBLE, 1, 102, MPI_COMM_WORLD);
+    }
+    if (rank == 1)
+    {
+        double b;
+        MPI_Recv(&b, 1, MPI_DOUBLE, 0, 102, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+        std::cout << "Proces 1 odebral liczbe: " << b << std::endl;
+    }
+
+    MPI_Finalize();
+    return 0;
+}
