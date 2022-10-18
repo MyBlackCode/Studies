8e578b75717f8869a26c963172d4fa9092be4746
MyBlackCode <paula.mlynarczyk@op.pl>
2022-10-18 17:31:36 +0200

    created 17:31 18.10.2022

---
+++ ParallelSystemsAndAlgorithms/PSAA_L1_T1.cpp
@@ -0,0 +1,18 @@
+//Zad1
+//Napisac program z wykorzystaniem biblioteki MPI, który pozwoli na
+//uruchomienie równolegle np procesów.Kazdy proces powinien wyswietlic
+//swój numer procesu oraz liczbe wszystkich uruchomionych procesów.
+
+#include <iostream>
+#include <mpi.h>
+
+int main(int argc, char* argv[]){
+	MPI_Init(&argc, &argv);
+	int rank, size;
+
+	MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+	MPI_Comm_size(MPI_COMM_WORLD, &size);
+
+	std::cout << "Jestem procesem nr: " << rank << " z: " << size << std::endl;
+	MPI_Finalize();
+}
