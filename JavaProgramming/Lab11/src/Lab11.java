d2fb6b1d157ed6eb94226d2fc3067a17db985977
MyBlackCode <paula.mlynarczyk@op.pl>
2022-12-28 22:43:31 +0100

    22:43 28.12.2022

--- JavaProgramming/Lab11/src/Lab11.java
+++ JavaProgramming/Lab11/src/Lab11.java
@@ -1,18 +1,30 @@
 import java.io.IOException;
+import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.util.DoubleSummaryStatistics;
-import java.util.LongSummaryStatistics;
+import java.util.List;
 import java.util.Map;
+import java.util.Scanner;
 import java.util.TreeMap;
 import java.util.regex.Pattern;
 import java.util.stream.Collectors;
+import java.util.Optional;
 
 public class Lab11 {
+	 public static Optional<Long> countWords(Long value) {
+	        return value == 0 ? Optional.empty() : Optional.of(value);
+	    }
+	 
 	static public void main(String[] args) throws IOException {
+		
+		var contents = new String(Files.readAllBytes(Paths.get("BajkaCK.txt")),StandardCharsets.UTF_8);
+		List <String> words = List.of(contents.replaceAll(",", " ").replace(".", " ").replace("!", " ").replace("?", " ").split("\\s+"));
+		
 		Pattern pattern = Pattern.compile("\\s+");
 		Map<String,Long> wordsCounts = Files.lines(Paths.get("BajkaCK.txt"))
-										.flatMap(line -> pattern.splitAsStream(line))
+										.flatMap(line -> pattern.splitAsStream(
+												line.replaceAll(",", " ").replace(".", " ").replace("!", " ").replace("?", " ")))
 										.collect(Collectors.groupingBy(String :: toUpperCase, 
 												TreeMap::new, Collectors.counting()));
 		
@@ -25,16 +37,64 @@ public class Lab11 {
 						});
 		
 
-		LongSummaryStatistics stats = wordsCounts.entrySet().stream().mapToLong(x -> x.getValue()).summaryStatistics();
-		DoubleSummaryStatistics sumarry = wordsCounts.entrySet().stream().mapToDouble(x -> x.getKey().length() ).summaryStatistics();
-		long numberOfWords = stats.getCount();
-		System.out.printf("\nLiczba slow: "+ numberOfWords);
-		double avg = sumarry.getAverage();
-		System.out.printf("\nSrednia dlugosc slowa: " + avg);
-		double min = sumarry.getMin();
-		System.out.printf("\nMinimalna dlugosc slowa: "+ min);
-		double max = sumarry.getMax();
-		System.out.printf("\nMaksymalna dlugosc slowa : "+ max);
+		DoubleSummaryStatistics summary1 = wordsCounts.entrySet().stream().mapToDouble(x -> x.getValue()).summaryStatistics();
+		double numberOfWords = summary1.getSum();
+		System.out.print("\n\nLiczba slow: "+ numberOfWords);
+		double distinctNumberOfWords = summary1.getCount();
+		System.out.print("\nLiczba roznych slow: "+ distinctNumberOfWords);
+		
+		DoubleSummaryStatistics summary3 = wordsCounts.entrySet().stream().mapToDouble(x -> x.getValue()*x.getKey().length() ).summaryStatistics();
+		double avg = summary3.getSum()/numberOfWords;
+		System.out.print("\nSrednia dlugosc slowa: " + avg);
+		
+		DoubleSummaryStatistics summary4 = wordsCounts.entrySet().stream().mapToDouble(x -> x.getKey().length() ).summaryStatistics();
+		double min = summary4.getMin();
+		System.out.print("\nMinimalna dlugosc slowa: "+ min);
+		double max = summary4.getMax();
+		System.out.print("\nMaksymalna dlugosc slowa : "+ max);
+		
+		
+	
+        
+		System.out.println("\n\nPierwsze 10 słów tekstu (+ dlugosc poszczegolnych slow)");
+		words.stream().limit(10).forEach(w -> System.out.print(w + " "));
+		System.out.print("\n");
+		words.stream().limit(10).forEach(w -> System.out.print(w.length() + " "));
+		
+		System.out.println("\n\nOstatnie 10 słów tekstu (+ dlugosc poszczegolnych slow)");
+		words.stream().skip(words.size()-10).forEach(w -> System.out.print(w + " "));
+		System.out.printf("\n");
+		words.stream().skip(words.size()-10).forEach(w -> System.out.print(w.length() + " "));
+		
+		System.out.println("\n\nDlugosc Liczba_wystapien");
+		wordsCounts.entrySet().stream().collect(
+				Collectors.groupingBy(entry -> entry.getKey().length(), TreeMap::new,
+						Collectors.toList())).forEach((l, wordLengthList)->{
+							System.out.printf("\n"+l+"      "+wordLengthList.stream().mapToLong(word -> word.getValue()).sum());
+						});
+		
+		System.out.print("\n\nSuma (laczna liczba liter): ");
+		System.out.println(words.stream().mapToInt(String::length).sum());
+		
+		Scanner scanner = new Scanner(System.in);
+        System.out.print("Podaj szukana fraze nr 1 (wielkosc liter nie ma znaczenia) : ");
+        String phrase1 = scanner.next();
+        Optional<Long> results =  countWords(words.stream().map(String::toUpperCase).filter(s -> s.contains(phrase1.toUpperCase())).count());
+        
+ 		results.ifPresentOrElse(e -> {System.out.print("Fraza: %s wystepuje w tekscie"+ results.get() +"razy.\nLista slow. ");
+ 		words.stream().map(String::toUpperCase).filter(s -> s.contains(phrase1.toUpperCase())).forEach(s -> System.out.print(s + " "));}, 
+ 		() -> System.out.printf("Fraza "+phrase1+" NIE wystwpuje w tekscie"));
+ 		
+ 		System.out.print("\n\nPodaj szukana fraze nr 2 (wielkosc liter nie ma znaczenia) : ");
+        String phrase2 = scanner.next();
+        Optional<Long> results2 =  countWords(words.stream().map(String::toUpperCase).filter(s -> s.contains(phrase2.toUpperCase())).count());
+         
+  		results2.ifPresentOrElse(e -> {System.out.print("Fraza: %s wystepuje w tekscie"+ results2.get() +"razy.\nLista slow. ");
+  		words.stream().map(String::toUpperCase).filter(s -> s.contains(phrase2.toUpperCase())).forEach(s -> System.out.print(s + " "));}, 
+  		() -> System.out.printf("Fraza "+phrase2+" NIE wystwpuje w tekscie"));
+        
+        
+        scanner.close();
 	}
 
 }
